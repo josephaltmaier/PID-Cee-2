7ab8b7c7d054a2cfd3e223565f53c01934280c5a
joseph <joseph.altmaier@gmail.com>
2020-07-21 16:28:50 -0700

    Create a test server which will accept incoming bluetooth connections, try to parse an incoming message into a protobuf, and respond with a protobuf

---
+++ rpi/src/bluetooth_proto_test.py
@@ -0,0 +1,47 @@
+import bluetooth
+import threading
+from google.protobuf.any_pb2 import Any
+from generated.python.proto.bluetooth_pb2 import HelloWorld
+
+def receiveAndRespond(socket):
+    try:
+        data = client_sock.recv(1024)
+        print("Data received:", str(data))
+        testAny = Any()
+        testAny.ParseFromString(data)
+
+        testHW = HelloWorld()
+        testAny.Unpack(testHW)
+        responseMessage = data
+        if testHW.message:
+            print("Received proto message:", testHW.message)
+            responseMessage = testHW.message
+        else:
+            print("Received non-proto message")
+
+        responseHW = HelloWorld()
+        responseHW.message = "Received message:" + responseMessage
+
+        responseWrapper = Any()
+        responseWrapper.Pack(responseHW)
+        responseBytes = responseWrapper.SerializeToString()
+        socket.send(responseBytes)
+    finally:
+        socket.close()
+
+server_sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
+
+port = 0x24067
+
+server_sock.bind(("", port))
+server_sock.listen(1)
+
+try:
+    while True:
+        client_sock, address = server_sock.accept()
+        print("Accepted connection from", address)
+        t = threading.Thread(target=receiveAndRespond)
+        t.setDaemon(True)
+        t.start()
+finally:
+    server_sock.close()
