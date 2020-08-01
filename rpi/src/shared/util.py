57da980a3ac2eee3463779d8c9f63aca996aae84
joseph <joseph.altmaier@gmail.com>
2020-08-01 12:35:00 -0700

    Python3 needs explicit byte conversion in struct.pack

--- rpi/src/shared/util.py
+++ rpi/src/shared/util.py
@@ -7,5 +7,5 @@ def get_ip_address(ifname):
     return socket.inet_ntoa(fcntl.ioctl(
         s.fileno(),
         0x8915,  # SIOCGIFADDR
-        struct.pack('256s', bytes(ifname[:15]))
+        struct.pack('256s', bytes(ifname[:15], "utf-8"))
     )[20:24])
